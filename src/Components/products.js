d4696e69d158ea80af2bd7bcd420f784e2e2be18
Shivani Borhara <shivaniborhara@shivanis-mbp.lan>
2022-08-28 19:59:00 -0400

    product page start

--- src/Components/products.js
+++ src/Components/products.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 const ProductList = () => {
 	const getProducts = async () => {
 		console.log('help');
 		try {
-			const response = await axios.get(
-				'http://localhost:8000/products'
-			);
+			const response = await axios.get('http://localhost:8000/products');
 
 			console.log(response);
 		} catch (error) {
@@ -19,7 +18,20 @@ const ProductList = () => {
 		getProducts();
 	});
 
-	return <h1>All Products</h1>;
+	return (
+		<Card border="info" style={{ width: '18rem' }}>
+			<Card.Img variant="top" src="pexels-lilartsy-3335613.jpg" />
+			<Card.Body>
+				<Card.Title>Coffee Mug</Card.Title>
+				<Card.Text>
+					Coffee Mugs available in various colors. Personalization available or
+					choose from one of many saying options.
+					<p>Price: $11.99</p>
+				</Card.Text>
+				<Button variant="primary">Go somewhere</Button>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default ProductList;
